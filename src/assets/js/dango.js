270a66eec710cd734945d562af5ca6ef0b4c38e5
Nurio Fernández <personal@nurio.me>
2021-10-14 23:22:39 +0200

    Improved dango easter egg

--- src/assets/js/dango.js
+++ src/assets/js/dango.js
@@ -2,7 +2,24 @@ let input = "";
 document.addEventListener('keypress',function(e){
     input += String.fromCharCode(e.keyCode);
     if(!input.endsWith("dango")) return;
+    playEffect();
+});
+
+function playEffect() {
+    const style = document.createElement("link");
+    style.setAttribute("rel", "stylesheet");
+    style.setAttribute("type", "text/css");
+    style.setAttribute("href", `https://${window.location.hostname}/assets/css/dango.css`);
+    document.getElementsByTagName("head")[0].appendChild(style);
+
+    const loader = document.createElement("div");
+    loader.setAttribute("class", "loader")
+    for(let i=0; i<=9; i++) {
+        loader.appendChild(document.createElement("span"));
+    }
+    document.getElementsByTagName("body")[0].appendChild(loader);
+
 
     const audio = new Audio(`https://${window.location.hostname}/assets/audio/dango.mov`);
     audio.play();
-});
+}
