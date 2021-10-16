d8272dba9ff4f973b09621a318a1f08240f6f159
Nurio Fernández <personal@nurio.me>
2021-10-16 17:47:59 +0200

    Added spaces to the Reflected Event Handler project name

--- src/pages/projects/reflected-event-handler.php
+++ src/pages/projects/reflected-event-handler.php
@@ -9,7 +9,7 @@
 				</div>
 				<div class="col-md-5">
 					<div class="card-block" style="padding: 38px; min-height: 20rem;">
-						<h4 class="card-title">ReflectedEventHandler</h4>
+						<h4 class="card-title">Reflected Event Handler</h4>
 						<p class="card-text">Java Reflection Event System that allows you to create an event-driven architecture in an easy and beautiful way.</p>
 						<a href="https://github.com/xXNurioXx/ReflectedEventHandler/wiki" target="_blank" class="btn btn-primary mt-1">Go to the wiki</a>
 						<a href="https://github.com/xXNurioXx/ReflectedEventHandler" target="_blank" class="btn btn-info mt-1">Source code</a>
