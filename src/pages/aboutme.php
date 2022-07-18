ae32f7251d2b7d24356369a095abc3629f974ecb
Nurio Fernández <personal@nurio.me>
2022-07-18 03:14:48 +0200

    Simplified aboutme page skills section

--- src/pages/aboutme.php
+++ src/pages/aboutme.php
@@ -56,7 +56,7 @@
 					<p class="ml-1 mb-0 lh-100 badge badge-success">Native/C</p>
 					<p class="ml-1 mb-0 lh-100 badge badge-warning">Intermediate/B</p>
 					<p class="ml-1 mb-0 lh-100 badge badge-danger">Elemental/A</p>
-					<p class="ml-1 mb-0 lh-100 badge badge-secondary">Studying/Words</p>
+					<p class="ml-1 mb-0 lh-100 badge badge-secondary">Studying</p>
 				</div>
 			</div>
 
@@ -66,7 +66,6 @@
 				<p class="badge badge-success mb-0">Catalan</p>
 				<p class="badge badge-warning mb-0">English</p>
 				<p class="badge badge-secondary mb-0">Japanese</p>
-				<p class="badge badge-secondary mb-0">French</p>
 			</h5>
 		</div>
 
@@ -76,49 +75,23 @@
 			<!-- BLOCK : PANEL LEGEND -->
 			<div class="row ml-1 mr-1 mbM-2">
 				<div class="lh-100 p-1 mrM-auto">
-					<h5 class="text-uppercase mb-0 text-left-when-desktop-center-when-phone">Programming languages &amp; Tools</h5>
+					<h5 class="text-uppercase mb-0 text-left-when-desktop-center-when-phone">Favorite programming languages</h5>
 				</div>
 				<div class="lh-100 pt-0 ml-auto text-right-when-desktop-center-when-phone">
 					<p class="ml-1 mb-0 lh-100 badge badge-success">Advanced</p>
 					<p class="ml-1 mb-0 lh-100 badge badge-warning">Middle</p>
-					<p class="ml-1 mb-0 lh-100 badge badge-danger">Begginer</p>
-					<p class="ml-1 mb-0 lh-100 badge badge-secondary">Studying/Elemental</p>
+					<p class="ml-1 mb-0 lh-100 badge badge-danger">Beginner</p>
 				</div>
 			</div>
 
 			<!-- BLOCK : PANEL ITEMS -->
 			<h5 class="mb-0 text-left-when-desktop-center-when-phone">
-				<p class="badge badge-success mb-0">Java 7/8/11</p>
-				<p class="badge badge-success mb-0">Lombok</p>
-				<p class="badge badge-success mb-0">SSH</p>
-				<p class="badge badge-success mb-0">Bukkit</p>
-				<p class="badge badge-success mb-0">XPath</p>
-				<p class="badge badge-success mb-0">RegEx</p>
-				<p class="badge badge-success mb-0">SpringBoot</p>
-				<p class="badge badge-success mb-0">Hibernate</p>
-				<p class="badge badge-success mb-0">JUnit</p>
-				<p class="badge badge-success mb-0">MySQL</p>
-				<p class="badge badge-success mb-0">Postgres</p>
-				<p class="badge badge-success mb-0">Apache</p>
-				<p class="badge badge-success mb-0">Bash</p>
-				<p class="badge badge-success mb-0">HTML/CSS</p>
-				<p class="badge badge-warning mb-0">C++</p>
-				<p class="badge badge-warning mb-0">C</p>
-				<p class="badge badge-warning mb-0">C#</p>
-				<p class="badge badge-warning mb-0">Mockito</p>
-				<p class="badge badge-warning mb-0">PHP</p>
-				<p class="badge badge-warning mb-0">TypeScript</p>
-				<p class="badge badge-warning mb-0">JavaScript</p>
-				<p class="badge badge-warning mb-0">Bootstrap</p>
-				<p class="badge badge-warning mb-0">NodeJS</p>
-				<p class="badge badge-warning mb-0">ElectronJS</p>
-				<p class="badge badge-warning mb-0">ReactJS</p>
-				<p class="badge badge-warning mb-0">Deno</p>
-				<p class="badge badge-danger mb-0">GO</p>
-				<p class="badge badge-danger mb-0">Android SDK</p>
-				<p class="badge badge-danger mb-0">Python</p>
-				<p class="badge badge-danger mb-0">Pawn</p>
-				<p class="badge badge-danger mb-0">x86 Assembly</p>
+				<p class="badge badge-success mb-0">GO</p>
+				<p class="badge badge-success mb-0">TypeScript</p>
+				<p class="badge badge-success mb-0">JavaScript</p>
+				<p class="badge badge-success mb-0">Java 7/8/11/17</p>
+				<p class="badge badge-success mb-0">PHP</p>
+				<p class="badge badge-warning mb-0">C/C++</p>
 			</h5>
 
 		</div>
@@ -134,21 +107,20 @@
 				<div class="lh-100 pt-0 ml-auto text-right-when-desktop-center-when-phone">
 					<p class="ml-1 mb-0 lh-100 badge badge-success">Advanced</p>
 					<p class="ml-1 mb-0 lh-100 badge badge-warning">Middle</p>
-					<p class="ml-1 mb-0 lh-100 badge badge-danger">Begginer</p>
-					<p class="ml-1 mb-0 lh-100 badge badge-secondary">Studying/Elemental</p>
+					<p class="ml-1 mb-0 lh-100 badge badge-danger">Beginner</p>
 				</div>
 			</div>
 
 			<!-- BLOCK : PANEL ITEMS -->
 			<h5 class="mb-0 text-left-when-desktop-center-when-phone">
-				<p class="badge badge-success mb-0">Single Responsability Principle</p>
-				<p class="badge badge-success mb-0">Clean & commented programming</p>
-				<p class="badge badge-success mb-0">Event Driven Programming</p>
+				<p class="badge badge-success mb-0">Documentation</p>
+				<p class="badge badge-success mb-0">Simplicity</p>
+				<p class="badge badge-success mb-0">Single Responsibility Principle</p>
+				<p class="badge badge-success mb-0">Event Driven Development</p>
 				<p class="badge badge-success mb-0">Microkernel architecture</p>
 				<p class="badge badge-success mb-0">Encapsulation</p>
 				<p class="badge badge-success mb-0">Polymorphism</p>
-				<p class="badge badge-success mb-0">Modular programming</p>
-				<p class="badge badge-success mb-0">Asynchronous programming</p>
+				<p class="badge badge-success mb-0">Concurrency</p>
 				<p class="badge badge-success mb-0">Cryptography & security</p>
 				<p class="badge badge-success mb-0">Testing</p>
 				<p class="badge badge-success mb-0">Sockets</p>
@@ -157,11 +129,9 @@
 				<p class="badge badge-success mb-0">HTTP</p>
 				<p class="badge badge-success mb-0">SSL</p>
 				<p class="badge badge-success mb-0">Caching</p>
-				<p class="badge badge-success mb-0">L7 Firewall</p>
-				<p class="badge badge-warning mb-0">L3/L4 Firewall</p>
+				<p class="badge badge-success mb-0">L3/L4/L7 Firewall</p>
 				<p class="badge badge-warning mb-0">Assembly memory hacking</p>
 				<p class="badge badge-warning mb-0">Memory Hacking</p>
-				<p class="badge badge-warning mb-0">TDD</p>
 			</h5>
 
 		</div>
@@ -177,33 +147,28 @@
 				<div class="lh-100 pt-0 ml-auto text-right-when-desktop-center-when-phone">
 					<p class="ml-1 mb-0 lh-100 badge badge-success">Advanced</p>
 					<p class="ml-1 mb-0 lh-100 badge badge-warning">Middle</p>
-					<p class="ml-1 mb-0 lh-100 badge badge-danger">Begginer</p>
-					<p class="ml-1 mb-0 lh-100 badge badge-secondary">Studying/Elemental</p>
+					<p class="ml-1 mb-0 lh-100 badge badge-danger">Beginner</p>
 				</div>
 			</div>
 
 			<!-- BLOCK : PANEL ITEMS -->
 			<h5 class="mb-0 text-left-when-desktop-center-when-phone">
 				<p class="badge badge-success mb-0">Git</p>
-				<p class="badge badge-success mb-0">IntelliJ IDEA</p>
+				<p class="badge badge-success mb-0">Docker</p>
+				<p class="badge badge-success mb-0">Jetbrains IDEs</p>
 				<p class="badge badge-success mb-0">Visual Studio Code</p>
-				<p class="badge badge-success mb-0">Windows Terminal</p>
-				<p class="badge badge-success mb-0">FileZilla/WinSCP</p>
 				<p class="badge badge-success mb-0">Ubuntu Server 12-20</p>
 				<p class="badge badge-success mb-0">MacOS Mojave-BigSur</p>
 				<p class="badge badge-success mb-0">Windows 7-10</p>
-				<p class="badge badge-warning mb-0">Maven</p>
-				<p class="badge badge-warning mb-0">CLion</p>
+				<p class="badge badge-success mb-0">Maven</p>
+				<p class="badge badge-success mb-0">NodeJs</p>
+				<p class="badge badge-success mb-0">Adobe XD</p>
+				<p class="badge badge-success mb-0">Draw.io</p>
 				<p class="badge badge-warning mb-0">Cheat Engine</p>
 				<p class="badge badge-warning mb-0">Adobe Lightroom</p>
 				<p class="badge badge-warning mb-0">Adobe Photoshop</p>
 				<p class="badge badge-warning mb-0">Adobe Premiere</p>
 				<p class="badge badge-warning mb-0">AudaCity</p>
-				<p class="badge badge-warning mb-0">NPM</p>
-				<p class="badge badge-danger mb-0">Visual Studio</p>
-				<p class="badge badge-danger mb-0">Adobe AfterEffects</p>
-				<p class="badge badge-danger mb-0">Android Studio</p>
-				<p class="badge badge-danger mb-0">Unity3D</p>
 			</h5>
 
 		</div>
